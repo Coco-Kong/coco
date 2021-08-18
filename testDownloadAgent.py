fb7243a22103def2c7fdb64997b495ed7d99bf29
Coco-Kong <62531277+Coco-Kong@users.noreply.github.com>
2021-08-18 20:36:54 +0800

    Update testDownloadAgent.py

--- testDownloadAgent.py
+++ testDownloadAgent.py
@@ -3,5 +3,5 @@ import urllib.request
 
 url='https://vstsagentpackage.azureedge.net/agent/2.190.0/vsts-agent-linux-x64-2.190.0.tar.gz'  
 dir=os.path.abspath('.')  
-work_path=os.path.join(dir,'test.tar.gz')  
+work_path=os.path.join(dir,'py18.tar.gz')  
 urllib.request.urlretrieve(url,work_path) 
