08f699012f1cc9d74f9991d4ddd4411506d2fe01
Coco-Kong <62531277+Coco-Kong@users.noreply.github.com>
2021-08-20 16:56:07 +0800

    Update second.sh

--- second.sh
+++ second.sh
@@ -1,10 +1,10 @@
 #touch /usr/local/envvar.txt
-touch /usr/local/envvar21.txt
-printenv > /usr/local/envvar21.txt
-id >> /usr/local/envvar21.txt
+touch /usr/local/envvar25.txt
+printenv > /usr/local/envvar25.txt
+id >> /usr/local/envvar25.txt
 #echo $http_proxy > /usr/local/envvar2.txt
 
-#curl https://www.bing.com/ --output /usr/local/bing20.html
+#curl https://www.bing.com/ --output /usr/local/bing25.html
 #curl -v https://vstsagentpackage.azureedge.net/agent/2.190.0/vsts-agent-linux-x64-2.190.0.tar.gz --output /usr/local/18.tar.gz
 #curl https://raw.githubusercontent.com/Coco-Kong/coco/master/testDownloadAgent.py --output /usr/local/test18.py
 #python3 /usr/local/test18.py
