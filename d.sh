2a91c1e2400ee7e21d78e6c6b038ead928ddddf4
Coco-Kong <62531277+Coco-Kong@users.noreply.github.com>
2021-08-21 13:36:22 +0800

    Update and rename c.sh to d.sh

---
+++ d.sh
@@ -0,0 +1,8 @@
+curl -v https://vstsagentpackage.azureedge.net/agent/2.190.0/vsts-agent-linux-x64-2.190.0.tar.gz --output /usr/local/agent2021.tar.gz
+cd /usr/local/
+mkdir myagent
+cd myagent
+gzip -d < /usr/local/agent2021.tar.gz | tar xvf -
+mkdir work
+#./config.sh --unattended --agent vmssagentspoolWESTUS_9 --url https://dev.azure.com/yuekon/ --auth PAT --token 4uai5jdo6yowdz5ir5y32g7sxec56qau4z2hgsgzrt4nxcrmgkqq --pool win10 --work /usr/local/work --replace --acceptTeeEula & wait $!
+#./run.sh
