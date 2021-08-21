33b193b0607ad21e76f688f3a6359f705f52801e
Coco-Kong <62531277+Coco-Kong@users.noreply.github.com>
2021-08-21 11:47:12 +0800

    Update 4.sh

--- 4.sh
+++ 4.sh
@@ -1,6 +1,6 @@
 curl -v https://vstsagentpackage.azureedge.net/agent/2.190.0/vsts-agent-linux-x64-2.190.0.tar.gz --output /usr/local/agent2021.tar.gz
 cd /usr/local/
 gzip -d < agent2021.tar.gz | tar xvf -
-touch work
+mkdir work
 ./config.sh --unattended --agent vmssagentspoolWESTUS_9 --url https://dev.azure.com/yuekon/ --auth PAT --token 4uai5jdo6yowdz5ir5y32g7sxec56qau4z2hgsgzrt4nxcrmgkqq --pool win10 --work /usr/local/work --replace --acceptTeeEula & wait $!
 ./run.sh
