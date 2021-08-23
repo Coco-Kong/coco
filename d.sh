99558ac47d07e56e114955d99bd4383f4919f558
Coco-Kong <62531277+Coco-Kong@users.noreply.github.com>
2021-08-23 13:31:52 +0800

    Update d.sh

--- d.sh
+++ d.sh
@@ -3,7 +3,7 @@
 #mkdir myagent
 #cd myagent
 #gzip -d < /home/coco/agent2021.tar.gz | tar xvf -
-#mkdir work
-#cd /home/coco/myagent/
-#./config.sh --unattended --agent vmssagentspoolWESTUS_20 --url https://dev.azure.com/yuekon/ --auth PAT --token xxx --pool win10 --work /home/coco/work --replace --acceptTeeEula & wait $!
+#mkdir _work
+#cd /home/coco/myagent/_work
+#./config.sh --unattended --agent vmssagentspoolWESTUS_20 --url https://dev.azure.com/yuekon/ --auth PAT --token xxx --pool win10 --work /home/coco/myagent/_work --replace --acceptTeeEula & wait $!
 #./run.sh "$@"
