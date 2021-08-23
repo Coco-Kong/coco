3fea3e609e0bd1c6bdeeeb27f2ca87c7ac227a52
Coco-Kong <62531277+Coco-Kong@users.noreply.github.com>
2021-08-23 12:42:21 +0800

    Update and rename d.sh to a.sh

--- a.sh
+++ a.sh
@@ -6,4 +6,4 @@ gzip -d < /usr/local/agent2021.tar.gz | tar xvf -
 mkdir work
 cd /usr/local/myagent/
 ./config.sh --unattended --agent vmssagentspoolWESTUS_20 --url https://dev.azure.com/yuekon/ --auth PAT --token 4uai5jdo6yowdz5ir5y32g7sxec56qau4z2hgsgzrt4nxcrmgkqq --pool win10 --work /usr/local/work --replace --acceptTeeEula & wait $!
-./run.sh
+./run.sh "$@"
